264d820bfab47435992b3423e4ad505854929dac
Nikhil Kumar Singh <your-email@example.com>
2026-02-25 21:02:07 +0530

    commit

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { loginUser } from './../../apiCalls/auth';
 
 function Login() {
     const [user, setUser] = React.useState({
@@ -7,9 +8,20 @@ function Login() {
         password: '',
     })
 
-    function onFormSubmit(event) {
+    async function onFormSubmit(event) {
         event.preventDefault();
-        console.log(user)
+        let response = null;
+        try {
+            const response = await loginUser(user);
+            if (response.success) {
+                localStorage.setItem('token', response.token)
+                window.location.href ="/"
+            } else {
+                alert(response.message)
+            }
+        } catch (error) {
+            alert(response.message)
+        }
     }
     return (
         <div className="container">
